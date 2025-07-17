ba23340bd7c24e811e1878c7283d671a3f836a5f
Bartlomiej Bogdan Szarlej <bartlomiej-bogdan.szarlej@dolphin-it.de>
2025-07-17 12:52:04 +0200

    make methods optional

--- lib/src/state.dart
+++ lib/src/state.dart
@@ -1,16 +1,7 @@
-final class AnyState<T> extends State<T> {
-  @override
-  void onEnter(T owner, [State<T>? from]) {}
-
-  @override
-  void onExit(T owner, [State<T>? to]) {}
-
-  @override
-  void onUpdate(double dt, T owner) {}
-}
+final class AnyState<T> extends State<T> {}
 
 abstract class State<T> {
-  void onEnter(T owner, [State<T>? from]);
-  void onExit(T owner, [State<T>? to]);
-  void onUpdate(double dt, T owner);
+  void onEnter(T owner, [State<T>? from]) {}
+  void onExit(T owner, [State<T>? to]) {}
+  void onUpdate(double dt, T owner) {}
 }
